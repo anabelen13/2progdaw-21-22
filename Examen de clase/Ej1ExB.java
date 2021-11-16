27c441c1227a2248492253eb0dd4a0f435638e0c
wittynyx <anabelen.gil.fp@iescampanillas.es>
2021-11-16 12:42:41 +0100

    Create Ej1ExB.java

---
+++ Examen de clase/Ej1ExB.java
@@ -0,0 +1,21 @@
+
+
+public class Ej1ExB {
+	
+	public static void main (String[] args) {
+	int latidosPorMinuto;
+	int anios;
+	
+	long latidosTotales;
+	
+	System.out.print("Introduzca el tiempo en años: ");
+	anios = Integer.parseInt(System.console().readLine());
+	
+	System.out.print("Introduzca su ritmo cardiaco medio")
+	
+	latidosTotales = (long)anios * 365 * 24 *60*latidosPorMinuto;
+	
+	System.out.print("En ese tiempo, su corazón habrá latido %d veces.\n",latidos Totales);	
+	}
+}
+
