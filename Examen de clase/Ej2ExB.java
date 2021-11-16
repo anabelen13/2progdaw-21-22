63f5971800951c70c4c94297cb47e506f7720d51
wittynyx <anabelen.gil.fp@iescampanillas.es>
2021-11-16 12:42:46 +0100

    Create Ej2ExB.java

---
+++ Examen de clase/Ej2ExB.java
@@ -0,0 +1,32 @@
+//Piramide porosa pa abajo
+
+public class Ej2ExA {
+	
+	public static void main (String[] args) {
+		String caracter;
+		int altura;
+		
+		System.out.print("Introduzca la altura de la priamide: ");
+		altura = Integer.parseInt(System.console().readLine());
+		
+		System.out.print("Introduzca el carácter de rellenos: ");
+		caracter = Integer.aprseInt(System.console().readLine());
+		
+		int huecos=0;
+		int secuencia = altura-1;
+		
+		System.out.println();
+		for (int i = 0; i < altura; i++){
+			for (int j = 0; j < huecos; j++){
+				System.out.print(" ");
+			}
+			System.out.print(caracter);
+			for (int j = 0; j < secuencia; j++){
+				System.out.print(" "+ caracter);
+			}
+			System.out.println();
+			huecos++;
+			secuencia--;
+		}
+	}
+}
