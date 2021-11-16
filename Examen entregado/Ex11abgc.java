397c29f9820cb5a345c19c9bc1095ea3efa3656e
wittynyx <anabelen.gil.fp@iescampanillas.es>
2021-11-16 12:42:28 +0100

    Create Ex11abgc.java

---
+++ Examen entregado/Ex11abgc.java
@@ -0,0 +1,13 @@
+//11. Ana Belén Gil Cortés, 08/11/2021
+//Programa que traduzca los numeros PIN de los digitos introducidos por el usuario.
+
+public class sin título {
+	
+	public static void main (String[] args) {
+		
+    System.out.print("Introduzca el PIN: ");
+    int pin = Interger.parseInt(System.console().readLine());
+    
+    
+	}
+}
