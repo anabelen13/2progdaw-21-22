53bb9b3b029931ca80c169a7622232d5b50f6f59
wittynyx <anabelen.gil.fp@iescampanillas.es>
2021-11-16 12:42:26 +0100

    Create Ex11abgc1.java

---
+++ Examen entregado/Ex11abgc1.java
@@ -0,0 +1,17 @@
+//11. Ana Belén Gil Cortés, 08/11/2021
+//Programa que calcule el número de latidos del corazón de una persona durante varios años.
+//Se le solicitará al usuario el tiempo transcurrido, en años, y el ritmo cardiaco medio.
+
+public class Ex11abgc1 {
+	
+	public static void main (String[] args) {
+		
+    System.out.print("Introduzca su rítmo cardíaco medio en pulsaciones por minuto: ");
+    int pulsaciones = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Introduzca el tiempo en años: ");
+    int años = Integer.parseInt(System.console().readLine());
+    
+	}
+}
+
