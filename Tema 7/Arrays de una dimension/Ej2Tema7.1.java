fded1b552b6eb9a7b1bd4d066627d2dcd6d516a6
wittynyx <anabelen.gil.fp@iescampanillas.es>
2021-11-17 12:40:17 +0100

    Create Ej2Tema7.1.java

---
+++ Tema 7/Arrays de una dimension/Ej2Tema7.1.java
@@ -0,0 +1,17 @@
+
+public class SEj2Tema7.1 {
+	public static void main(String[] args) {
+		
+		char[] simbolo = new char[10];
+		simbolo[0] = 'a';
+		simbolo[1] = 'x';
+		simbolo[4] = '@';
+		simbolo[6] = ' ';
+		simbolo[7] = '+';
+		simbolo[8] = 'Q';
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("simbolo[" + i + "]: " + simbolo[i]);
+		}
+	}
+}
