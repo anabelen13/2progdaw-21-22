a30b863dfbde4853d734eae100af7f36ade89419
wittynyx <anabelen.gil.fp@iescampanillas.es>
2021-11-17 12:08:13 +0100

    Create Ej19Tema6.java

---
+++ Tema 6/Ej19Tema6.java
@@ -0,0 +1,25 @@
+
+public class Ej19Tema6 {
+	public static void main(String[] args) {
+		
+		int numero;
+		int maximoPar = -100;
+		int minimoImpar = 200;
+		int suma = 0;
+		
+		for (int i = 0; i < 50; i++) {
+			numero = (int)(Math.random() * 301) - 100;
+			System.out.print(numero + " ");
+			
+			if (numero % 2 == 0) { // el número es par
+				if (numero > maximoPar) maximoPar = numero;
+			} else { // el número es impar
+				if (numero < minimoImpar) minimoImpar = numero;
+			}
+			suma += numero;
+		}
+		System.out.println("\nMáximo de los pares: " + maximoPar);
+		System.out.println("Mínimo de los impares: " + minimoImpar);
+		System.out.println("Media: " + suma / 50);
+	}
+}
